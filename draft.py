a64e9a5efb6293a0afd2180959d15d0d23ce1498
Sergey Chirkov <s.chirkov1@g.nsu.ru>
2025-11-09 21:57:46 +0700

    5.5 is done

--- draft.py
+++ draft.py
@@ -19,7 +19,9 @@ for n in range(2, 9):
     new_list.append(input_list[n] + input_list[n+1])
 
 print(new_list)
-'''
+
+from os.path import split
+
 #2nd
 input_text = 'Enter numbers through space -->'
 input_list = [1.1]
@@ -35,4 +37,45 @@ while int(input_list[0]) != input_list[0]:
 new_list = [el for el in input_list if el != 3]
 print(new_list)
 
+
 #3rd
+import re
+
+text = list(input('enter your text -->').split())
+for word in range(len(text)):
+    text[word] = re.sub(r'[!-/:-@\[-`{-~]', r'', text[word])
+
+print(text)
+
+
+#4rd
+import re
+
+text = list(input('enter your text -->').split())
+for word in range(len(text)):
+    text[word] = re.sub(r'[!-/:-@\[-`{-~]', r'', text[word])
+
+print(list(set(text)))
+
+import random as rand
+
+import numpy
+
+
+def chance_machine() -> list:
+    exit_list = []
+    for num in range(rand.randint(1, 20)):
+        exit_list.append(rand.randrange(100))
+    return exit_list
+
+l = chance_machine()
+print(l, numpy.mean(l))
+'''
+#6th
+d = []
+number = int(input('Enter one number -->'))
+for divs in range(int(number**0.5)+1):
+    if number % divs == 0:
+        d.append(divs)
+
+
