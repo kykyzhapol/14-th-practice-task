28257c4ccecfbaeeeeef4d0335b86e90559fcd98
Sergey Chirkov <s.chirkov1@g.nsu.ru>
2025-11-11 17:24:27 +0700

    All is ready (this is miracle?!)

--- draft.py
+++ draft.py
@@ -111,7 +111,7 @@ def count_even_odd(rand_list: list) -> None:
 
 
 count_even_odd(chance_machine())
-'''
+
 import re
 
 text_list = []
@@ -131,3 +131,100 @@ sorted_list = sorted(count_dict.items(), key=lambda el: el[1], reverse=True)
 
 for word in sorted_list:
     print(word[0])
+
+#10th
+lst1 = []
+while len(lst1) == 0:
+    try:
+        lst1 = input('enter first list thought space -->').split()
+    except ValueError:
+        print('please, enter minimum 1 number argument')
+
+lst2 = []
+while len(lst2) == 0:
+    try:
+        lst2 = input('enter second list thought space -->').split()
+    except ValueError:
+        print('please, enter minimum 1 number argument')
+
+
+diapason = []
+while len(diapason) != 2:
+    try:
+        diapason = [int(n) for n in input('enter 2 numbers -->').split()]
+    except ValueError:
+        print('please, enter two numbers')
+
+inter_list = lst1[diapason[0]-1:diapason[1]]
+del lst1[diapason[0]-1:diapason[1]]
+inter_list.reverse()
+lst2 += inter_list
+
+print(lst2, lst1)
+
+
+#11th
+lst = []
+while len(lst) == 0:
+    try:
+        lst = [int(n) for n in input('enter list thought space -->').split()]
+    except ValueError:
+        print('Please, enter elements correct')
+
+in_command = ''
+while len(in_command) == 0:
+    in_command = input('Enter command Rn or Ln, where n - parameter -->')
+    try:
+        # Исправлена проверка команды
+        if in_command[0] not in ['R', 'L'] or not in_command[1:].isdigit():
+            print('Please, enter command correct')
+            in_command = ''
+    except IndexError:
+        print('Please, enter command correct')
+
+# Извлекаем направление и количество шагов
+direction = in_command[0]
+steps = int(in_command[1:])
+
+# Выполняем сдвиг
+if direction == 'R':  # Сдвиг вправо
+    steps = steps % len(lst)  # Нормализуем количество шагов
+    exit_lst = lst[-steps:] + lst[:-steps]
+else:  # Сдвиг влево
+    steps = steps % len(lst)  # Нормализуем количество шагов
+    exit_lst = lst[steps:] + lst[:steps]
+
+print(exit_lst)
+
+#12th
+dot_letters = ['a','b','d','e','g','o','p','q']
+
+input_words = [word.lower() for word
+            in input('enter words thought space and lower case -->').split()]
+
+
+dot_counter = 0
+normal_letter_counter = 0
+dot_counter_w = 0
+normal_letter_counter_w = 0
+owerdoted_words = []
+
+for word in input_words:
+    dot_counter_w = 0
+    normal_letter_counter_w = 0
+    for letter in word:
+        if letter in dot_letters:
+            dot_counter_w += 1
+        else:
+            normal_letter_counter_w += 1
+    # Убрано else и исправлено условие
+    if dot_counter_w >= 2:  # Исправлено: dot_counter_w вместо dot_counter
+        owerdoted_words.append(word)
+    dot_counter += dot_counter_w
+    normal_letter_counter += normal_letter_counter_w
+
+
+print(f'dotted letters - {dot_counter}, '
+      f'normal letter - {normal_letter_counter} '
+      f'list of owerdoted words : {owerdoted_words}')
+'''
