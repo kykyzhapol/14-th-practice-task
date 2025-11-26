4119a878791bd2ff56a3bc6db011fb3246987fb7
Sergey Chirkov <s.chirkov1@g.nsu.ru>
2025-11-26 13:13:05 +0700

    Update 11_ex.py

--- 11_ex.py
+++ 11_ex.py
@@ -16,7 +16,6 @@ def import_data() -> list:
     while len(in_command) == 0:
         in_command = input('Enter command Rn or Ln, where n - parameter -->')
         try:
-            # Исправлена проверка команды
             if in_command[0] not in ['R', 'L'] or not in_command[1:].isdigit():
                 print('Please, enter command correct')
                 in_command = ''
@@ -33,16 +32,14 @@ def main() -> None:
     '''
     imported_data = import_data()
     lst = imported_data[0]
-    # Извлекаем направление и количество шагов
     direction = imported_data[1][0]
     steps = int(imported_data[1][1:])
 
-    # Выполняем сдвиг
-    if direction == 'R':  # Сдвиг вправо
-        steps = steps % len(lst)  # Нормализуем количество шагов
+    if direction == 'R':  
+        steps = steps % len(lst)
         exit_lst = lst[-steps:] + lst[:-steps]
-    else:  # Сдвиг влево
-        steps = steps % len(lst)  # Нормализуем количество шагов
+    else:
+        steps = steps % len(lst) 
         exit_lst = lst[steps:] + lst[:steps]
 
     print(exit_lst)
